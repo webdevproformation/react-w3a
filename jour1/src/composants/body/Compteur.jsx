c02adbe936e0e3e43cae4d37ef9dc4cf3bf096ac
Malik H <malik.h@webdevpro.net>
2021-03-31 16:09:38 +0200

    sfc

--- jour1/src/composants/body/Compteur.jsx
+++ jour1/src/composants/body/Compteur.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+/* import { Component } from 'react';
 
 class Compteur extends Component {
     state = { 
@@ -9,11 +9,11 @@ class Compteur extends Component {
         // alert("j'ai cliqué sur le bouton +");
         // console.log(this);
         // this.state.valeur++; 
-        /* let valeur = this.state.valeur ; */
-        /* let that = this; */
-       /*  this.setState({
-            valeur : this.state.valeur + 1 
-        }) */
+        // let valeur = this.state.valeur ; 
+        // let that = this; 
+        //  this.setState({
+        //    valeur : this.state.valeur + 1 
+        // }) 
         // appel une méthode qui est stockée dans le parent 
         this.props.augmenter( data );
     }
@@ -56,6 +56,49 @@ class Compteur extends Component {
             </div>
         );
     }
+}
+ 
+export default Compteur; */
+
+//sfc stateless fonctional component
+
+
+const diminuer = (data) => {
+    //console.log(this.state.valeur);
+    //this.setState({ valeur : this.state.valeur - 1 });
+    this.props.diminuer(data);
+}
+
+function passage(){
+    console.log("je viens de rentrer dans le bouton +");
+}
+
+const info = (param) => {
+    console.log(param);
+}
+
+function augmenter( data ){
+    this.props.augmenter( data );
+}
+
+function misEnformeCompteur( valeur ){
+    if(valeur === 0){
+        return "Zero" ;
+    }
+    return valeur ;
+}
+
+const Compteur = props => {
+    return ( 
+        <div className="compteur" style={{ padding: "0.2em 0" }}>
+                <span> 
+                    { misEnformeCompteur( props.compteur.valeur ) }
+                </span>
+                <button onClick={ () => { augmenter( props.compteur ) } } onMouseEnter={passage}>+</button>
+                <button onClick={() => { diminuer( props.compteur )}}>-</button>
+                <button onClick={ () => { info( "une information" ) }}>information</button>
+        </div>
+    );
 }
  
 export default Compteur;
